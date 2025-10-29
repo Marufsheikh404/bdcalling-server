69ab417eebc357e804d56e8dfca4b4ea9c31a5e4
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-29 16:07:25 +0600

    first commit

--- index.js
+++ index.js
@@ -11,11 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 
-app.post('jwt', async(req,res)=>{
+app.post('/jwt', async(req,res)=>{
     const user = req.body;
     const token = jwt.sign(user, process.env.SECREAT_KEY ,{expiresIn:"1h"});
-    res.send(token)
+    res.send({accessToken:token})
 })
+
+// veryToken middleware
+ const verifyToken = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader) return res.status(401).send({message:'unauthorized'});
+
+    const token = authHeader.split("")[1];
+    jwt.verify(token,process.env.SECREAT_KEY,(error,decoded)=>{
+        if(error) return res.status(403).send({message:'forbidden'});
+        req.decoded = decoded;
+        next()
+    });
+ };
 
 app.get('/',(req,res)=>{
     res.send('Bdcalling is running')
